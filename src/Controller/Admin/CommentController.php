f8f2ef78c98936b63d8d981e9c59aa26b64ac413
Slam <slam@mail.ru>
2022-08-23 21:16:23 +0300

    Реализовал управление коментариями

--- src/Controller/Admin/CommentController.php
+++ src/Controller/Admin/CommentController.php
@@ -5,10 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Comment;
 use App\Entity\Spot;
 use App\Repository\CommentRepository;
-use App\Repository\SpotRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -16,25 +14,32 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class CommentController extends AbstractController
 {
-    private SpotRepository $spotRepository;
-
     private CommentRepository $commentRepository;
 
-    public function __construct(SpotRepository $spotRepository, CommentRepository $commentRepository)
+    public function __construct(CommentRepository $commentRepository)
     {
-        $this->spotRepository = $spotRepository;
         $this->commentRepository = $commentRepository;
     }
 
     #[Route("admin/spots/{id<\d+>}/comments", name: "admin_spot_comments", methods: ["GET"])]
-    public function edit(Spot $spot, Request $request)
+    public function edit(Spot $spot)
     {
+        $comments = $this->commentRepository->findBy(['spot' => $spot]);
 
+        return $this->render(
+            'admin/comments/comments.html.twig', [
+            'spot' => $spot,
+            'comments' => $comments,
+        ]);
     }
 
     #[Route("admin/comments/{id<\d+>}/destroy", name: "admin_destroy_spot_comment", methods: ["GET", "DELETE"])]
     public function destroy(Comment $comment)
     {
+        $this->commentRepository->remove($comment, true);
 
+        $this->addFlash('message', 'Комментарий успешно удален');
+
+        return $this->redirectToRoute('admin_spot_comments', ['id' => $comment->getSpot()->getId()]);
     }
 }
