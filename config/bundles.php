03de1d371fb8338005d3e8b00e8adffd30c64691
Slam <slam@mail.ru>
2022-08-28 18:48:24 +0300

    jwt authentication bundle

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
