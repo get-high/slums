3949fd790f46e89ae095fa8549036e8c735a277d
Slam <slam@mail.ru>
2022-08-28 22:35:20 +0300

    Смаунтил реакт

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,20 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+//import './styles/app.css';
 
 // start the Stimulus application
-import './bootstrap';
+//import './bootstrap';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Hello From React</div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
---
+++ src/Controller/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Spot;
+use App\Service\SpotService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    private SpotService $spotService;
+
+    public function __construct(SpotService $spotService)
+    {
+        $this->spotService = $spotService;
+    }
+
+    /**
+     * @Route("/admin", name="admin_index")
+     */
+    public function index(Request $request)
+    {
+        return $this->render('layouts/admin.html.twig');
+    }
+}
